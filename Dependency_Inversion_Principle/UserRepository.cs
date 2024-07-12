df6cae2b76882aff1eee5a2c795c8c827c4c9ee6
Tiago França <t.iago_196@hotmail.com>
2024-07-12 13:18:10 -0300

    Adicionar arquivos de projeto.

---
+++ Dependency_Inversion_Principle/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion_Principle
+{
+    internal class UserRepository : IUserRepository
+    {
+        public string GetUser(int id)
+        {
+            //Any logic to get user from db
+            return "User data";
+        }
+    }
+}
---
+++ Dependency_Inversion_Principle/UserService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dependency_Inversion_Principle
+{
+    internal class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public void DisplayUser(int  userId)
+        {
+            var user = _userRepository.GetUser(userId);
+            Console.WriteLine(user);
+        }
+    }
+}
